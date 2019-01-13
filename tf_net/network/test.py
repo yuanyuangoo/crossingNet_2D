f0f05ed8cb5415a3f3b83264bcd9fa7fcf17ebcb
yuanyuangoo <yuanyuangoo@hotmail.com>
2019-01-14 10:28:57 +1300

    113

--- tf_net/network/test.py
+++ tf_net/network/test.py
@@ -6,38 +6,59 @@ from keras.models import Model, model_from_json
 from keras.applications.resnet50 import ResNet50
 from keras.activations import hard_sigmoid
 
-
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 sys.path.append('./')
-import globalConfig
-from data.dataset import *
 from data.util import *
+from data.dataset import *
+import globalConfig
 
-def vnect(train_dataset, valid_dataset):
-    epochs = 400
-    batch_size = 64
+input_size = 224
+output_size = 28
+
+
+def getData(train_dataset, valid_dataset, batch_size):
     _, train_skel, _, train_img_rgb, n_samples, total_batch = prep_data(
         train_dataset, batch_size)
     _, test_skel, _, test_img_rgb, _, _ = prep_data(
         valid_dataset, batch_size)
     print("Preparing heatmap!")
-    input_size = 224
-    output_size = 28
+
     n_joints = 17
     train_heat_map = np.zeros(
         (n_samples, output_size, output_size, n_joints))
+    test_heat_map = np.zeros(
+        (batch_size, output_size, output_size, n_joints))
     # train_z_heat_maps = np.zeros(
     #     (n_samples, output_size, output_size, n_joints))
     for idx in tqdm(range(n_samples)):
         train_heat_map[idx], _ = SkelGaussianHeatMap(
             input_size, output_size, 224*(train_skel[idx]*2-1))
 
+    for idx in tqdm(range(test_skel.shape[0])):
+        test_heat_map[idx], _ = SkelGaussianHeatMap(
+            input_size, output_size, 224*(test_skel[idx]*2-1))
+
     train_heat_maps = []
     train_heat_maps.append(train_heat_map)
     train_heat_maps.append(train_heat_map)
-
     print("Prepare heatmap completed!")
+    return train_heat_maps, train_img_rgb, test_img_rgb, train_skel, test_skel, n_samples, total_batch
+
+
+def load_model():
+    # load json and create model
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    head_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    head_model.load_weights("model.h5")
+    print("Loaded model from disk")
+    return head_model
+
+
+def build_model():
     #load ResNet50 without dense layer and with theano dim ordering
     base_model = ResNet50(weights='imagenet', include_top=False,
                           input_shape=(224, 224, 3))
@@ -51,12 +72,14 @@ def vnect(train_dataset, valid_dataset):
 
     outputs = []
 
-    res4d_heatmap1a = Conv2DTranspose(17, 4, strides=2, padding='same', activation='sigmoid', name='res4d_heatmap1a')(res4d.output)
+    res4d_heatmap1a = Conv2DTranspose(
+        17, 4, strides=2, padding='same', activation='sigmoid', name='res4d_heatmap1a')(res4d.output)
     res4d_heatmap_bn = BatchNormalization(
         name='res4d_heatmap_bn')(res4d_heatmap1a)
     outputs.append(res4d_heatmap_bn)
 
-    res5a_heatmap1a = Conv2DTranspose(17, 4, strides=4, padding='same', activation='sigmoid', name='res5a_heatmap1a')(res5a.output)
+    res5a_heatmap1a = Conv2DTranspose(
+        17, 4, strides=4, padding='same', activation='sigmoid', name='res5a_heatmap1a')(res5a.output)
     res5a_heatmap_bn = BatchNormalization(
         name='res5a_heatmap_bn')(res5a_heatmap1a)
     outputs.append(res5a_heatmap_bn)
@@ -64,13 +87,10 @@ def vnect(train_dataset, valid_dataset):
     #create graph of your new model
     head_model = Model(input=base_model.input, output=outputs)
 
-    #compile the model
-    head_model.compile(optimizer='rmsprop',
-                       loss='mean_squared_error', metrics=['accuracy'])
-    head_model.fit(x=train_img_rgb, y=train_heat_maps,
-                   batch_size=64, epochs=epochs)
-    scores = head_model.evaluate(train_img_rgb, train_heat_maps, verbose=0)
-    print("%s: %.2f%%" % (head_model.metrics_names[1], scores[1]*100))
+    return head_model
+
+
+def save_model(head_model):
     # serialize model to JSON
     model_json = head_model.to_json()
     with open("model.json", "w") as json_file:
@@ -79,22 +99,31 @@ def vnect(train_dataset, valid_dataset):
     head_model.save_weights("model.h5")
     print("Saved model to disk")
 
-    # # load json and create model
-    # json_file = open('model.json', 'r')
-    # loaded_model_json = json_file.read()
-    # json_file.close()
-    # head_model = model_from_json(loaded_model_json)
-    # # load weights into new model
-    # head_model.load_weights("model.h5")
-    # print("Loaded model from disk")
 
-    head_model.compile(loss='mean_squared_error',
-                       optimizer='rmsprop', metrics=['accuracy'])
+def vnect(train_dataset, valid_dataset, resume_train=True):
+    epochs = 1
+    batch_size = 64
+
+    train_heat_maps, train_img_rgb, test_img_rgb, train_skel, test_skel, n_samples, total_batch = getData(
+        train_dataset, valid_dataset, batch_size)
+    if resume_train:
+        head_model = load_model()
+    else:
+        head_model = build_model()
+
+    #compile the model
+    head_model.compile(optimizer='rmsprop',
+                       loss='mean_absolute_error', metrics=['accuracy'])
+    head_model.fit(x=train_img_rgb, y=train_heat_maps,
+                   batch_size=64, epochs=epochs)
+
+    save_model(head_model)
+
     predict = head_model.predict(test_img_rgb)
     skel = SkelFromHeatmap(predict[1], predict[1], input_size)
     sample_dir = './'
-    save_images(test_img_rgb, image_manifold_size(batch_size),
-                '{}/test_{:02d}_{:04d}.png'.format(sample_dir, epochs, idx), skel=(skel+input_size)/(input_size*2))
+    save_images(test_img_rgb, image_manifold_size(batch_size), '{}/test_{:02d}_{:04d}.png'.format(
+        sample_dir, epochs, total_batch), skel=(skel+input_size)/(input_size*2))
     return 1
 
 
@@ -104,7 +133,7 @@ if __name__ == '__main__':
         ds = Dataset()
         # ds.loadH36M_expended(64*10, mode='train',
         #                      tApp=True, replace=False)
-        ds.loadH36M(64*100, mode='train',
+        ds.loadH36M(64*1, mode='train',
                     tApp=True, replace=False)
         val_ds = Dataset()
         # val_ds.loadH36M_all('all', mode='valid',
